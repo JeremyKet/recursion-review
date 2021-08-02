7e8691c8f22629cf18d1e60f43fbb6332f284f9d
Jeremy Ketema <jeremyketema@gmail.com>
2021-08-02 15:06:06 -0400

    review completed

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,31 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = '';
+  if (obj === undefined) {
+    return 'undefined';
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)){
+    for(var i = 0; i < obj.length; i++){
+      result += stringifyJSON(obj[i]) + ',';
+    }
+    return '[' + result.slice(0, result.length-1) +']'
+  }
+  if (typeof obj === 'object') {
+    for (var key in obj) {
+      if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+        result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+      }
+    }
+    return '{' + result.slice(0, result.length - 1) + '}';
+  }
+  return obj.toString();
 };
+
+
